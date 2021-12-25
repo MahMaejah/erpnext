429c05a085ceb5f754ecd88c3984a75d8f00ff39
MahMaejah <chipohameja@gmail.com>
2021-12-25 09:08:55 +0000

    Shifted client script

--- erpnext/education/doctype/testimonial/testimonial.js
+++ erpnext/education/doctype/testimonial/testimonial.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Testimonial', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		var chrts=[
+		    "Character and general behavior",
+            "Initiative, common sense and leadership quality",
+            "Sense of duty and punctuality",
+            "Co-operation with others",
+            "Attitude towards work",
+            "Intelligence and judgement",
+            "Dependability and Reliability"
+		    ];
+		    //console.log(frm.doc.testimonial_tb.length);
+			if(frm.doc.testimonial_tb.length <=1){
+				for(let td of chrts){
+				let row = frm.add_child('testimonial_tb',{
+					characters: td
+				});
+			}
+		}
+	}
 });
